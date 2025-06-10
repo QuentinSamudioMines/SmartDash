4414202ecf1c5f1385e15f34e332045bec476c75
Kiyou <quentin.samudio@yahoo.fr>
2025-06-10 17:17:45 +0200

    less heavy files

--- cut.py
+++ cut.py
@@ -3,14 +3,19 @@ import pandas as pd
 # Chargement des données
 city = pd.read_pickle("city_file.pkl")
 
+survivor = ["total_energy_consumption_basic", "Consommation par m² par an (en kWh/m².an)_basic", "total_energy_consumption_renovated", "energie_imope", "heating_efficiency"]
+
+# Filtrer et ne garder que la colonne 'survivor'
+city_survivor = city[survivor]
+
 # Calcul de la taille de chaque découpe
-n = len(city)
+n = len(city_survivor)
 chunk_size = n // 3
 
 # Découpage
-city_part1 = city.iloc[:chunk_size]
-city_part2 = city.iloc[chunk_size:2*chunk_size]
-city_part3 = city.iloc[2*chunk_size:]
+city_part1 = city_survivor.iloc[:chunk_size]
+city_part2 = city_survivor.iloc[chunk_size:2*chunk_size]
+city_part3 = city_survivor.iloc[2*chunk_size:]
 
 # Sauvegarde
 city_part1.to_pickle("city_part1.pkl")
@@ -18,3 +23,4 @@ city_part2.to_pickle("city_part2.pkl")
 city_part3.to_pickle("city_part3.pkl")
 
 print("Découpage terminé : city_part1.pkl, city_part2.pkl, city_part3.pkl créés.")
+
